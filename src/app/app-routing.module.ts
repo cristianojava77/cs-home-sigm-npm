21ed5295bf5df36b4ac02b681da533b1422c9f31
Cristiano Souza <cristiano.sigm@gmail.com>
2020-06-01 19:44:12 -0300

    Creating state service

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PersonListComponent } from './person-list/person-list.component';
 import { PersonCreateComponent } from './person-create/person-create.component';
-import { PersonReadComponent } from './person-read/person-read.component';
 import { PersonUpdateComponent } from './person-update/person-update.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'person', pathMatch: 'full' },
   { path: 'person-list', component: PersonListComponent },
   { path: 'person-create', component: PersonCreateComponent },
-  { path: 'person-read/:id', component: PersonReadComponent },
   { path: 'person-update/:id', component: PersonUpdateComponent }
 ];
 
---
+++ src/app/model/state.ts
@@ -0,0 +1,7 @@
+export class State {
+    id: number;
+	countrycode: number;
+	abbreviation: string;
+	title: string;
+    active: boolean;
+}
